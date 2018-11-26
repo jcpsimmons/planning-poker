ae905addc11d2502c816e21ddd791e0bf17476d0
Omar Mohamad - El Hassan Lopesino <omar.lopesino@metadrop.net>
2018-11-26 17:47:34 +0100

    Improve start.sh, showing better messages

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
-docker-compose up -d --build
+echo "Starting planning poker..."
+docker-compose up -d --build &>/dev/null
 sleep 2
-docker-compose run busybox-curl curl http://ngrok:4040/api/tunnels | grep -Po "https"://[^\"]+
-
+URL=$(docker-compose run busybox-curl curl http://ngrok:4040/api/tunnels | grep -Po "https"://[^\"]+)
+echo "Planning poker is up! Url: $URL"
+docker-compose rm -fsv busybox_curl &>/dev/null
