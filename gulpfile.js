11f64b0518e22be4884e538c9b9bd7e29a603511
Josh Simmons <jsimmons@anduril.com>
2023-08-11 11:47:14 -0700

    new env

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ const settings = {
   img: {
     min: false,
     input: theme_base_path + 'images/*',
-    output: theme_base_path + 'dist/images',
+    output: './dist/images',
   },
 };
 
@@ -27,37 +27,14 @@ const settings = {
  */
 const bs = require('browser-sync');
 const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const plumber = require('gulp-plumber');
-const gulpSass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const dartSass = require('sass');
 
-const { series, watch } = gulp;
+const { series } = gulp;
 
 const browserSync = bs.create();
-const sass = gulpSass(dartSass);
 
 /**
  * Gulp Tasks
  */
-
-// SASS.
-const buildSass = () => {
-  return gulp
-    .src(settings.styles.input)
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        errLogToConsole: true,
-        outputStyle: 'expanded', // expanded, compact, compressed
-      }).on('error', sass.logError)
-    )
-    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(settings.styles.output));
-};
 
 // Minify images.
 const buildImages = () => {
@@ -74,26 +51,7 @@ const serve = () => {
     port: 3001,
     open: false,
     proxy: settings.server.proxy,
-    // injectChanges: true
   });
 };
 
-//   gulp.watch(theme_base_path + 'css/*.css', browserSync.reload);
-//   gulp.watch(theme_base_path + 'js/**/*.js', browserSync.reload);
-// });
-
-// // Default task.
-// gulp.task('default', ['serve', 'sass'], function () {
-//   console.log('here')
-//   gulp.watch(settings.styles.input, ['sass']);
-
-//   if (settings.js.es6) {
-//     gulp.watch(settings.js.input, ['es6']);
-//   }
-
-//   if (settings.img.min) {
-//     gulp.watch(settings.img.input, ['imgmin']);
-//   }
-// });
-
-exports.build = series(buildSass, buildImages, buildJS, serve);
+exports.build = series(buildImages, buildJS, serve);
--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-echo "Starting planning poker..."
-docker-compose up -d --build &>/dev/null
-sleep 2
-URL=$(docker-compose run busybox-curl curl http://ngrok:4040/api/tunnels | grep -Po "https"://[^\"]+)
-echo "Planning poker is up! Url: $URL"
-docker-compose rm -fsv busybox_curl &>/dev/null
